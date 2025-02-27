ecac56727df63686f13829f5725da5a3d73baab8
José Eduardo <dev.souzaedu@gmail.com>
2025-02-27 12:36:02 -0300

    2122

---
+++ frontend/src/app/calculator/page.tsx
@@ -0,0 +1,11 @@
+// src/app/calculator/page.tsx
+"use client";
+
+export default function CalculatorPage() {
+  return (
+    <div>
+      <h1>Página da Calculadora</h1>
+      <p>Coming soon</p>
+    </div>
+  );
+}
