a8e0eb17b5ac7edc73ba17b524240f46456cea4f
José Eduardo <dev.souzaedu@gmail.com>
2025-02-27 12:38:16 -0300

    wife thots

---
+++ frontend/src/app/strava/page.tsx
@@ -0,0 +1,11 @@
+// src/app/strava/page.tsx
+"use client";
+
+export default function StravaPage() {
+  return (
+    <div>
+      <h1>Página Strava</h1>
+      <p>Conteúdo relacionado ao Strava aqui.</p>
+    </div>
+  );
+}
