d8497dbf36477609ddeac83d6e807ddbf5b90312
José Eduardo <dev.souzaedu@gmail.com>
2025-02-28 11:35:10 -0300

    keks

---
+++ frontend/src/head.tsx
@@ -0,0 +1,19 @@
+// src/app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>Gerador de unhas com IA / O Canivete-suiço para nail designers</title>
+        <meta
+          name="description"
+          content="Crie designs de unhas incríveis com a nossa ferramenta de IA. O Canivete-suiço para nail designers."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* Apple Icon para homescreen */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* Outras meta tags podem ser adicionadas aqui */}
+      </>
+    );
+  }
+  
