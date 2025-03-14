c2f45a17c3944e261af6217eb0f2b56568209af1
José Eduardo <dev.souzaedu@gmail.com>
2025-03-14 14:27:33 -0300

    wipp

--- frontend/src/app/strava-for/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request) {
-  // Redirecionar para a página principal
-  return NextResponse.rewrite(new URL('/strava-for', request.url));
-} 
